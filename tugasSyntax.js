826a316aa2977c024a94c2b25c39f4cc003efcd0
unknown <Hadiramadan376@gmail.com>
2023-09-19 14:00:34 +0700

    menambahkan code js

---
+++ tugasSyntax.js
@@ -0,0 +1,13 @@
+// Buat 3 variabel
+let var1 = 10;
+let var2 = 'saya belajar javascript';
+let var3 = 20;
+
+//menjumlahkan variable1 dan variable2
+let hasilPenjumlahan = var1 + var2;
+if(hasilPenjumlahan = isNaN){
+  hasilPenjumlahan = var1 + var3 
+}
+ 
+// Tampilkan hasil penjumlahan
+console.log("hasil penjumblahanya adalah =" + hasilPenjumlahan)
